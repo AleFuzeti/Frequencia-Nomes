d150c655284269d7f274cbf3e52bc4cd006b8e8b
Matheus Kenzo <matheus.kenzo.tuda@uel.br>
2023-10-09 03:40:45 -0300

    script_refinament
    adicionando novamente o get regiões após teste

--- scripts/src/Main.java
+++ scripts/src/Main.java
@@ -13,6 +13,50 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            // URL da API para obter as macrorregiões
+            String regioesUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes";
+
+            // Criação da URL e conexão para obter as macrorregiões
+            URL regioesObj = new URL(regioesUrl);
+            HttpURLConnection regioesCon = (HttpURLConnection) regioesObj.openConnection();
+            regioesCon.setRequestMethod("GET");
+
+            // Leitura da resposta da API para obter as macrorregiões
+            BufferedReader regioesIn = new BufferedReader(new InputStreamReader(regioesCon.getInputStream()));
+            StringBuilder regioesResponse = new StringBuilder();
+            String regioesInputLine;
+
+            while ((regioesInputLine = regioesIn.readLine()) != null) {
+                regioesResponse.append(regioesInputLine);
+            }
+
+            regioesIn.close();
+
+            // Configurar o ObjectMapper para a formatação com identação
+            ObjectMapper prettyObjectMapper = new ObjectMapper();
+            prettyObjectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            // Converter a resposta JSON em um array de objetos JSON
+            JsonNode regioesJsonArray = prettyObjectMapper.readTree(regioesResponse.toString());
+
+            // Caminho para a pasta de saída (uma pasta acima de src)
+            String outputFolderPath = "../output";
+
+            // Verificar se a pasta de saída existe e criá-la se não existir
+            File outputFolder = new File(outputFolderPath);
+            if (!outputFolder.exists()) {
+                outputFolder.mkdirs();
+            }
+
+            // Salvar os dados das macrorregiões em um arquivo JSON com identação
+            String regioesOutputPath = outputFolderPath + "/regioes.json";
+            FileWriter regioesFileWriter = new FileWriter(regioesOutputPath);
+            ObjectWriter regioesWriter = prettyObjectMapper.writerWithDefaultPrettyPrinter();
+            regioesWriter.writeValue(regioesFileWriter, regioesJsonArray);
+            regioesFileWriter.close();
+
+            System.out.println("Dados das macrorregiões salvos em " + regioesOutputPath);
+
             // URL da API para obter os estados de todas as macrorregiões (IDs 1, 2, 3, 4, 5)
             String estadosUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes/1%7C2%7C3%7C4%7C5/estados";
 
@@ -33,20 +77,10 @@ public class Main {
             estadosIn.close();
 
             // Configurar o ObjectMapper para a formatação com identação
-            ObjectMapper prettyObjectMapper = new ObjectMapper();
             prettyObjectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
             // Converter a resposta JSON em um array de objetos JSON
             JsonNode estadosJsonArray = prettyObjectMapper.readTree(estadosResponse.toString());
-
-            // Caminho para a pasta de saída (uma pasta acima de src)
-            String outputFolderPath = "../output";
-
-            // Verificar se a pasta de saída existe e criá-la se não existir
-            File outputFolder = new File(outputFolderPath);
-            if (!outputFolder.exists()) {
-                outputFolder.mkdirs();
-            }
 
             // Salvar os dados dos estados de todas as macrorregiões em um único arquivo JSON com identação
             String estadosOutputPath = outputFolderPath + "/estados_todas_regioes.json";
