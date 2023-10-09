7ff859c49b44e74cc4db7b43eab4413e7c875d77
Matheus Kenzo <matheus.kenzo.tuda@uel.br>
2023-10-09 02:15:54 -0300

    last_get_data_localapi
    adicionamos o ultimo get da API de localizações do IBGE e guardamos os dados brutos em arquivos.json (regiões, estados de cada região e todos os municípios de cada estados) na pasta output :)

--- scripts/src/Main.java
+++ scripts/src/Main.java
@@ -13,50 +13,6 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            // URL da API para obter as macrorregiões
-            String regioesUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes";
-
-            // Criação da URL e conexão para obter as macrorregiões
-            URL regioesObj = new URL(regioesUrl);
-            HttpURLConnection regioesCon = (HttpURLConnection) regioesObj.openConnection();
-            regioesCon.setRequestMethod("GET");
-
-            // Leitura da resposta da API para obter as macrorregiões
-            BufferedReader regioesIn = new BufferedReader(new InputStreamReader(regioesCon.getInputStream()));
-            StringBuilder regioesResponse = new StringBuilder();
-            String regioesInputLine;
-
-            while ((regioesInputLine = regioesIn.readLine()) != null) {
-                regioesResponse.append(regioesInputLine);
-            }
-
-            regioesIn.close();
-
-            // Configurar o ObjectMapper para a formatação com identação
-            ObjectMapper prettyObjectMapper = new ObjectMapper();
-            prettyObjectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-
-            // Converter a resposta JSON em um array de objetos JSON
-            JsonNode regioesJsonArray = prettyObjectMapper.readTree(regioesResponse.toString());
-
-            // Caminho para a pasta de saída (uma pasta acima de src)
-            String outputFolderPath = "../output";
-
-            // Verificar se a pasta de saída existe e criá-la se não existir
-            File outputFolder = new File(outputFolderPath);
-            if (!outputFolder.exists()) {
-                outputFolder.mkdirs();
-            }
-
-            // Salvar os dados das macrorregiões em um arquivo JSON com identação
-            String regioesOutputPath = outputFolderPath + "/regioes.json";
-            FileWriter regioesFileWriter = new FileWriter(regioesOutputPath);
-            ObjectWriter regioesWriter = prettyObjectMapper.writerWithDefaultPrettyPrinter();
-            regioesWriter.writeValue(regioesFileWriter, regioesJsonArray);
-            regioesFileWriter.close();
-
-            System.out.println("Dados das macrorregiões salvos em " + regioesOutputPath);
-
             // URL da API para obter os estados de todas as macrorregiões (IDs 1, 2, 3, 4, 5)
             String estadosUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes/1%7C2%7C3%7C4%7C5/estados";
 
@@ -76,14 +32,69 @@ public class Main {
 
             estadosIn.close();
 
+            // Configurar o ObjectMapper para a formatação com identação
+            ObjectMapper prettyObjectMapper = new ObjectMapper();
+            prettyObjectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            // Converter a resposta JSON em um array de objetos JSON
+            JsonNode estadosJsonArray = prettyObjectMapper.readTree(estadosResponse.toString());
+
+            // Caminho para a pasta de saída (uma pasta acima de src)
+            String outputFolderPath = "../output";
+
+            // Verificar se a pasta de saída existe e criá-la se não existir
+            File outputFolder = new File(outputFolderPath);
+            if (!outputFolder.exists()) {
+                outputFolder.mkdirs();
+            }
+
             // Salvar os dados dos estados de todas as macrorregiões em um único arquivo JSON com identação
             String estadosOutputPath = outputFolderPath + "/estados_todas_regioes.json";
             FileWriter estadosFileWriter = new FileWriter(estadosOutputPath);
             ObjectWriter estadosWriter = prettyObjectMapper.writerWithDefaultPrettyPrinter();
-            estadosWriter.writeValue(estadosFileWriter, prettyObjectMapper.readTree(estadosResponse.toString()));
+            estadosWriter.writeValue(estadosFileWriter, estadosJsonArray);
             estadosFileWriter.close();
 
             System.out.println("Dados de todos os estados salvos em " + estadosOutputPath);
+
+            // Criar uma lista de IDs de UFs a partir dos dados dos estados
+            StringBuilder ufIds = new StringBuilder();
+            for (JsonNode estado : estadosJsonArray) {
+                String ufId = estado.get("id").asText();
+                ufIds.append(ufId).append("|");
+            }
+            // Remover o último caractere "|" da lista
+            if (ufIds.length() > 0) {
+                ufIds.setLength(ufIds.length() - 1);
+            }
+
+            // URL da API para obter os municípios de todas as UFs
+            String municipiosUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + ufIds + "/municipios";
+
+            // Criação da URL e conexão para obter os municípios de todas as UFs
+            URL municipiosObj = new URL(municipiosUrl);
+            HttpURLConnection municipiosCon = (HttpURLConnection) municipiosObj.openConnection();
+            municipiosCon.setRequestMethod("GET");
+
+            // Leitura da resposta da API para obter os municípios de todas as UFs
+            BufferedReader municipiosIn = new BufferedReader(new InputStreamReader(municipiosCon.getInputStream()));
+            StringBuilder municipiosResponse = new StringBuilder();
+            String municipiosInputLine;
+
+            while ((municipiosInputLine = municipiosIn.readLine()) != null) {
+                municipiosResponse.append(municipiosInputLine);
+            }
+
+            municipiosIn.close();
+
+            // Salvar os dados dos municípios de todas as UFs em um único arquivo JSON com identação
+            String municipiosOutputPath = outputFolderPath + "/municipios_todas_ufs.json";
+            FileWriter municipiosFileWriter = new FileWriter(municipiosOutputPath);
+            ObjectWriter municipiosWriter = prettyObjectMapper.writerWithDefaultPrettyPrinter();
+            municipiosWriter.writeValue(municipiosFileWriter, prettyObjectMapper.readTree(municipiosResponse.toString()));
+            municipiosFileWriter.close();
+
+            System.out.println("Dados de todos os municípios salvos em " + municipiosOutputPath);
 
         } catch (Exception e) {
             e.printStackTrace();
