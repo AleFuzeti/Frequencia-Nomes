4e24b500dab7ee30a001872d9da90032e7faca68
Matheus Kenzo <matheus.kenzo.tuda@uel.br>
2023-11-21 05:38:33 -0300

    Update local_names_db.sql

--- coleta de dados/scripts/src/local_names_db.sql
+++ coleta de dados/scripts/src/local_names_db.sql
@@ -1,27 +1,24 @@
 create schema local_names_db;
 
 CREATE TABLE local_names_db.regiao (
-    id PRIMARY KEY,
+    id VARCHAR(10) PRIMARY KEY,
     sigla VARCHAR(2) UNIQUE,
-    nome VARCHAR(255) UNIQUE,
-    CONSTRAINT ck_id CHECK (id > 0)
+    nome VARCHAR(255) UNIQUE
 );
 
 CREATE TABLE local_names_db.estado (
-    id PRIMARY KEY,
+    id VARCHAR(10) PRIMARY KEY,
     sigla VARCHAR(2) UNIQUE NOT NULL,
     nome VARCHAR(255),
     sigla_reg VARCHAR(2) NOT NULL,
-    CONSTRAINT fk_sigla_reg FOREIGN KEY (sigla_reg) REFERENCES local_names_db.regiao(sigla),
-    CONSTRAINT ck_id_estado CHECK (id > 0)
+    CONSTRAINT fk_sigla_reg FOREIGN KEY (sigla_reg) REFERENCES local_names_db.regiao(sigla)
 );
 
 CREATE TABLE local_names_db.cidade (
-    id PRIMARY KEY,
+    id VARCHAR(10) PRIMARY KEY,
     nome VARCHAR(255) UNIQUE NOT NULL,
     sigla_estado VARCHAR(2) NOT NULL,
-    CONSTRAINT fk_sigla_estado FOREIGN KEY (sigla_estado) REFERENCES local_names_db.estado(sigla),
-    CONSTRAINT ck_id_cidade CHECK (id > 0)
+    CONSTRAINT fk_sigla_estado FOREIGN KEY (sigla_estado) REFERENCES local_names_db.estado(sigla)
 );
 
 CREATE TABLE local_names_db.nome (
@@ -30,7 +27,7 @@ CREATE TABLE local_names_db.nome (
     CONSTRAINT pk_names PRIMARY KEY (nome, localidade),
     frequencia INT DEFAULT 0,
     ranking INT DEFAULT 0,
-    CONSTRAINT fk_localidade_nome FOREIGN KEY (cidade) REFERENCES local_names_db.cidade(nome)
+    CONSTRAINT fk_localidade_nome FOREIGN KEY (localidade) REFERENCES local_names_db.cidade(nome)
 );
 
 -- Create table local_names_db.local_nome (
