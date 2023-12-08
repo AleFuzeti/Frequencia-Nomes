2955238f4998c265fafe061a09f2b7cc33016a70
AleFuzeti <ale.bertipaglia@uel.br>
2023-12-08 04:00:27 -0300

    Update LocalidadeControler.java

--- projeto/src/main/java/com/company/local_name/controller/LocalidadeControler.java
+++ projeto/src/main/java/com/company/local_name/controller/LocalidadeControler.java
@@ -43,15 +43,14 @@ public class LocalidadeControler {
     //     this.cidadeDAO = cidadeDAO;
     // }
 
+    private static final String DB_URL = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
+    private static final String DB_USER = "matheus";
+    private static final String DB_PASSWORD = "bd23";
+
     @GetMapping("/regioes")
     public List<Regiao> getRegioes() {
         List<Regiao> regioes = new ArrayList<>();
-
-        String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-        String user = "matheus";
-        String password = "bd23";
-
-        try (Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              Statement statement = connection.createStatement()) {
 
             String query = "SELECT * FROM local_names_db.regiao";
@@ -66,12 +65,11 @@ public class LocalidadeControler {
                 regioes.add(regiao);
             } else {
                 System.out.println("No region found");
-                populateRegioes(regioes);
+                regioes = populateRegioes(regioes);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return regioes;
     }
 
@@ -79,11 +77,7 @@ public class LocalidadeControler {
     public List<Estado> getEstados() {
         List<Estado> estados = new ArrayList<>();
 
-        String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-        String user = "matheus";
-        String password = "bd23";
-
-        try (Connection connection = DriverManager.getConnection(url, user, password);
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              Statement statement = connection.createStatement()) {
 
             String query = "SELECT * FROM local_names_db.estado";
@@ -98,27 +92,20 @@ public class LocalidadeControler {
                 );
                 estados.add(estado);
             } else {
-                System.out.println("No State found");
-                populateStates(estados);
+                System.out.println("No state found");
+                estados = populateStates(estados);
             }
-
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return estados;
     }
 
     @GetMapping("/cidades")
-    public List<Cidade> getCidades() {
+    public List<Cidade> getCidades(){
         List<Cidade> cidades = new ArrayList<>();
-
-        String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-        String user = "matheus";
-        String password = "bd23";
-
-        try (Connection connection = DriverManager.getConnection(url, user, password);
+        
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              Statement statement = connection.createStatement()) {
 
             String query = "SELECT * FROM local_names_db.cidade";
@@ -133,112 +120,94 @@ public class LocalidadeControler {
                 cidades.add(cidade);
             } else {
                 System.out.println("No city found");
-                populateCities(cidades);
+                cidades = populateCities(cidades);
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return cidades;
     }
 
-    public void populateRegioes(List<Regiao> regioes) {
+    public List<Regiao> populateRegioes(List<Regiao> regioes) {
         ObjectMapper prettyObjectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
         try{
             String regioesUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes";
             String regioesResponse = HttpUtil.fetchDataFromAPI(regioesUrl);
             JsonNode regioesJsonArray = prettyObjectMapper.readTree(regioesResponse);
-            System.out.println("Dados das macrorregiões: " + regioesJsonArray);
+            System.out.println("Dados das regiões corretos!! Tentativa de conectar ao banco.....");
 
             // inserir no banco de dados
             for (JsonNode regiao : regioesJsonArray) {
                 String id = regiao.get("id").asText();
-                String nome = regiao.get("nome").asText();
                 String sigla = regiao.get("sigla").asText();
-
-                String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-                String user = "matheus";
-                String password = "bd23";
-                try (Connection connection = DriverManager.getConnection(url, user, password)) {
-                    String sql = "INSERT INTO local_names_db.regiao (id, sigla, nome) VALUES (?, ?, ?)";
-
-                    try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                        statement.setString(1, id);
-                        statement.setString(2, sigla);
-                        statement.setString(3, nome);
-                    }
+                String nome = regiao.get("nome").asText();
+                
+                try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                    System.out.println("Conexão com o banco de dados estabelecida com sucesso! Tentando inserir....");
+                    insertRegiao(connection, id, sigla, nome);
                 } catch (SQLException e) {
+                    System.out.println("Erro ao conectar ao banco de dados");
                     e.printStackTrace();
                 }
-
-                Regiao regiaoo = new Regiao(id, nome, sigla);
+                Regiao regiaoo = new Regiao(id, sigla, nome);
                 //regiaoDAO.create(regiaoo);
                 regioes.add(regiaoo);
-            }
+            }        
+            return regioes;
         }
         catch(Exception e){
+            System.out.println("Erro ao buscar api de região");
             e.printStackTrace();
         }
+        return regioes;
     }
 
-    public void populateStates(List<Estado> estados){
+    public List<Estado> populateStates(List<Estado> estados){
         ObjectMapper prettyObjectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
         try{
             String estadosUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes/1%7C2%7C3%7C4%7C5/estados";
             String estadosResponse = HttpUtil.fetchDataFromAPI(estadosUrl);
             JsonNode estadosJsonArray = prettyObjectMapper.readTree(estadosResponse);
-            System.out.println("Dados dos estados: " + estadosJsonArray);
+            System.out.println("Dados dos estados corretos!! Tentativa de conectar ao banco.....");
 
-            // tratar dados e inserir no banco de dados
+            // tratar e inserir no banco de dados
             for (JsonNode estado : estadosJsonArray) {
                 String id = estado.get("id").asText();
                 String nome = estado.get("nome").asText();
                 String sigla = estado.get("sigla").asText();
-                String sigla_reg = estado.get("regiao").get("sigla").asText();
+                String siglaReg = estado.get("regiao").get("sigla").asText();
 
-                String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-                String user = "matheus";
-                String password = "bd23";
-                try (Connection connection = DriverManager.getConnection(url, user, password)) {
-                    String sql = "INSERT INTO local_names_db.estado (id, nome, sigla, sigla_reg) VALUES (?, ?, ?, ?)";
-                    try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                        statement.setString(1, id);
-                        statement.setString(2, nome);
-                        statement.setString(3, sigla);
-                        statement.setString(4, sigla_reg);
-                    }
+                try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                    System.out.println("Conexão com o banco de dados estabelecida com sucesso! Tentando inserir....");
+                    insertEstado(connection, id, nome, sigla, siglaReg);
                 } catch (SQLException e) {
+                    System.out.println("Erro ao conectar ao banco de dados");
                     e.printStackTrace();
                 }
-
-                Estado estadoo = new Estado(id, nome, sigla, sigla_reg);
+                Estado estadoo = new Estado(id, nome, sigla, siglaReg);
                 //estadoDAO.create(estadoo);
                 estados.add(estadoo);
             }
+            return estados;
         }
         catch(Exception e){
+            System.out.println("Erro ao buscar api de estados");
             e.printStackTrace();
         }
+        return estados;
     }
 
-    public void populateCities(List<Cidade> cidades){
+    public List<Cidade> populateCities(List<Cidade> cidades){
         ObjectMapper prettyObjectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
         try{
-            String url = "jdbc:postgresql://sicm.dc.uel.br:5432/matheus";
-            String user = "matheus";
-            String password = "bd23";
-
-            try (Connection connection = DriverManager.getConnection(url, user, password);
-                 Statement statement = connection.createStatement()) {
-
+            try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                Statement statement = connection.createStatement()) {
                 String query = "SELECT * FROM local_names_db.estado";
                 ResultSet resultSet = statement.executeQuery(query);
-
+                    
                 String ufIds = "";
                 while (resultSet.next()) {
-                    String ufId = resultSet.getString("id");
-                    ufIds += ufId + "|";
+                    ufIds += resultSet.getString("id") + "|";
                 }
                 // Remover o último caractere "|" da lista
                 if (ufIds.length() > 0) {
@@ -248,33 +217,78 @@ public class LocalidadeControler {
                 String municipiosUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/" + ufIds + "/municipios";
                 String municipiosResponse = HttpUtil.fetchDataFromAPI(municipiosUrl);
                 JsonNode municipiosJsonArray = prettyObjectMapper.readTree(municipiosResponse);
-                System.out.println("Dados dos municípios: " + municipiosJsonArray);
+                System.out.println("Dados dos municípios corretos!! Tentativa de conectar ao banco.....");
 
-                // tratar dados e inserir no banco de dados
+                // tratar e inserir no banco de dados
                 for (JsonNode municipio : municipiosJsonArray) {
                     String id = municipio.get("id").asText();
                     String nome = municipio.get("nome").asText();
-                    String sigla_estado = municipio.get("microrregiao").get("mesorregiao").get("UF").get("sigla").asText();
+                    String siglaEstado = municipio.get("microrregiao").get("mesorregiao").get("UF").get("sigla").asText();
 
-                    String sql = "INSERT INTO local_names_db.cidade (id, nome, sigla_estado) VALUES (?, ?, ?)";
-                    try (PreparedStatement statement2 = connection.prepareStatement(sql)) {
-                        statement2.setString(1, id);
-                        statement2.setString(2, nome);
-                        statement2.setString(3, sigla_estado);
-                    }
-                    catch (SQLException e) {
+                    try (Connection connection2 = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                        System.out.println("Conexão com o banco de dados estabelecida com sucesso! Tentando inserir....");
+                        insertCidade(connection2, id, nome, siglaEstado);
+                    } catch (SQLException e) {
+                        System.out.println("Erro ao conectar ao banco de dados");
                         e.printStackTrace();
                     }
-                    Cidade cidade = new Cidade(id, nome, sigla_estado);
+                    Cidade cidade = new Cidade(id, nome, siglaEstado);
                     //cidadeDAO.create(cidade);
                     cidades.add(cidade);
                 }
+                return cidades;
             }
             catch (SQLException e) {
+                System.out.println("Erro ao conectar ao banco de dados");
                 e.printStackTrace();
             }       
         }
         catch(Exception e){
+            System.out.println("Erro ao buscar api de municípios");
+            e.printStackTrace();
+        }
+        return cidades;
+    }
+
+    private static void insertRegiao(Connection connection, String id, String sigla, String nome) {
+        String sql = "INSERT INTO local_names_db.regiao (id, sigla, nome) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, id);
+            statement.setString(2, sigla);
+            statement.setString(3, nome);
+            statement.executeUpdate();
+            System.out.println("Região inserida com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir região");
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertEstado(Connection connection, String id, String nome, String sigla, String siglaReg) {
+        String sql = "INSERT INTO local_names_db.estado (id, nome, sigla, sigla_reg) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, id);
+            statement.setString(2, nome);
+            statement.setString(3, sigla);
+            statement.setString(4, siglaReg);
+            statement.executeUpdate();
+            System.out.println("Estado inserido com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir estado");
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertCidade(Connection connection, String id, String nome, String siglaEstado) {
+        String sql = "INSERT INTO local_names_db.cidade (id, nome, sigla_estado) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, id);
+            statement.setString(2, nome);
+            statement.setString(3, siglaEstado);
+            statement.executeUpdate();
+            System.out.println("Cidade inserida com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao inserir cidade");
             e.printStackTrace();
         }
     }
